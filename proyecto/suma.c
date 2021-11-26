51112143fdb965a75408bd94c5f7d7c7e8d5dfde
kokotr <AntonioPerezap077@gmail.com>
2021-11-26 11:53:23 -0600

    se creo una carpeta

---
+++ proyecto/suma.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+
+float a,b;
+
+printf("Dame un numero?:");
+scanf("%f",&a);
+
+printf("Dame otro numero?:");
+scanf("%f",&b);
+
+printf("el resultado de la suma es %.2f",a+b);
+
+
+
+
+
+
+
+return 0;
+}
+
